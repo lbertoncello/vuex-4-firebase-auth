72beff197978b47d6622de2e7ff54efc7c762595
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-11 18:24:26 -0300

    feat: add use signup action to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,30 @@
 import { createStore } from 'vuex'
+import { auth } from '../firebase/config'
+import {
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
 
 const store = createStore({
   state: {
     user: null
   },
+  // Do not allows async code
   mutations: {
     setUser(state, payload) {
       state.user = payload
       console.log('User state changed: ', state.user)
+    }
+  },
+  // Actions are just functions which allow async code
+  actions: {
+    async signup(context, { email, password }) {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      if (res) {
+        context.commit('setUser', { ...res.user, id: res.id })
+      } else {
+        throw new Error('Could not complete signup')
+      }
     }
   }
 })
