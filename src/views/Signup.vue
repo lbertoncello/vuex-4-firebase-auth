d6155ef2dd380be5dd4ca6a8d0b190774e77cdb7
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-11 18:24:54 -0300

    feat(Signuo): add user signup on firebase

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -9,22 +9,44 @@
     <input type="password" name="password" v-model="password" required>
 
     <button>Sign up</button>
+    <div v-if="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 export default {
   setup() {
+    // Vue native resources
+    const store = useStore()
+    const router = useRouter()
+
     const email = ref('')
     const password = ref('')
+    const error = ref(null)
 
-    const handleSubmit = () => {
-      console.log(email.value, password.value)
+    const handleSubmit = async () => {
+      try {
+        // The dispatch method is used to call an action method
+        await store.dispatch('signup', { 
+          email: email.value, 
+          password: password.value 
+        })
+        router.push({ name: 'Home' })
+      } catch (err) {
+        error.value = err.message
+      }
     }
 
-    return { handleSubmit, email, password }
+    return { 
+      email, 
+      password,
+      error,
+      handleSubmit
+    }
   }
 }
 </script>
