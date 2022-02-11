0f3528f8162d0de5099c3d9e71e64a2b711fb63a
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-11 17:59:45 -0300

    feat: add user to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,13 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: {
-    
+    user: null
   },
   mutations: {
-    
+    setUser(state, payload) {
+      state.user = payload
+      console.log('User state changed: ', state.user)
+    }
   }
 })
 
